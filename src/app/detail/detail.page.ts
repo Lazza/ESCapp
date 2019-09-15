0d03cdf5e388c1a7c10b169c85308bd4db8d691e
Andrea Lazzarotto <andrea.lazzarotto@gmail.com>
2019-09-15 21:17:04 +0200

    Display data from service

--- src/app/detail/detail.page.ts
+++ src/app/detail/detail.page.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Talk } from '../talk';
+import { DataService } from '../data.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-detail',
-  templateUrl: './detail.page.html',
-  styleUrls: ['./detail.page.scss'],
+    selector: 'app-detail',
+    templateUrl: './detail.page.html',
+    styleUrls: ['./detail.page.scss'],
 })
 export class DetailPage implements OnInit {
+    code: string;
+    sub: any;
 
-  constructor() { }
+    constructor(private route: ActivatedRoute, private dataService: DataService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            this.code = params['id'];
+        });
+    }
 
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+    get talk() {
+        return this.dataService.talks[this.code] || null;
+    }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+    constructor(private dataService: DataService) { }
+
+    get list() {
+        return this.dataService.list;
+    }
+
+    get talks() {
+        return this.dataService.talks;
+    }
 
 }
